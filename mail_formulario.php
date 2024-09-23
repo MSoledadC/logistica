24f5acd37200b1a24ccfd9df22e9f1c19bf63e5e
Soledad Cabanillas <mariasoledadcabanillas@hotmail.com>
2024-09-23 20:35:42 -0300

    se modifica responsive de banner, btn sumate, estilos

--- mail_formulario.php
+++ mail_formulario.php
@@ -13,17 +13,73 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $message = "Nombre: $nombre $apellido\nEmail: $email\nMensaje: $mensaje";
         $headers = "From: $email";
 
+        // Iniciar salida HTML
+        echo '<!DOCTYPE html>
+              <html lang="es">
+              <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Resultado del envío</title>
+                <link rel="stylesheet" href="css/cambios.css">
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900|Display+Playfair:200,300,400,700">
+                <link rel="stylesheet" href="fonts/icomoon/style.css">
+                <link rel="stylesheet" href="css/bootstrap.min.css">
+                <link rel="stylesheet" href="css/magnific-popup.css">
+                <link rel="stylesheet" href="css/jquery-ui.css">
+                <link rel="stylesheet" href="css/owl.carousel.min.css">
+                <link rel="stylesheet" href="css/owl.theme.default.min.css">
+                <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+                <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+                <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+                <link rel="stylesheet" href="css/aos.css">
+                <link rel="stylesheet" href="css/style.css">
+              </head>
+              <body>';
+
         // Enviar correo
         if (mail($to, $subject, $message, $headers)) {
-            echo "Mensaje enviado con éxito.<br>";
-            echo "<a href='index.html' class='btn btn-primary'>Volver al inicio</a>"; // Enlace al index
+            echo '<div class="container text-center mt-5">
+                    <p class="text-success">Mensaje enviado con éxito.</p>
+                    <a href="index.html" class="btn btn-primary py-3 px-5 empezar">Volver al inicio</a>
+                  </div>';
+            } else {
+                echo '<div class="container text-center mt-5">
+                        <p class="text-danger">Hubo un error al enviar el mensaje.</p>
+                        <a href="index.html" class="btn btn-primary py-3 px-5 empezar">Volver al inicio</a>
+                    </div>';
+            }
+
+            // Cerrar el HTML aquí
+            echo '</body></html>';
         } else {
-            echo "Hubo un error al enviar el mensaje.<br>";
-            echo "<a href='index.html' class='btn btn-primary'>Volver al inicio</a>"; // Enlace al index en caso de error
+            // Si faltan campos, devolver error
+            echo '<!DOCTYPE html>
+                <html lang="es">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Error en el envío</title>
+                    <link rel="stylesheet" href="css/cambios.css">
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900|Display+Playfair:200,300,400,700">
+                    <link rel="stylesheet" href="fonts/icomoon/style.css">
+                    <link rel="stylesheet" href="css/bootstrap.min.css">
+                    <link rel="stylesheet" href="css/magnific-popup.css">
+                    <link rel="stylesheet" href="css/jquery-ui.css">
+                    <link rel="stylesheet" href="css/owl.carousel.min.css">
+                    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+                    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+                    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+                    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+                    <link rel="stylesheet" href="css/aos.css">
+                    <link rel="stylesheet" href="css/style.css">
+                </head>
+                <body>
+                    <div class="container text-center mt-5">
+                        <p class="text-danger">Por favor, complete todos los campos.</p>
+                        <a href="index.html" class="btn btn-primary py-3 px-5 empezar">Volver al inicio</a>
+                    </div>
+                </body>
+                </html>';
         }
-    } else {
-        echo "Por favor, complete todos los campos.<br>";
-        echo "<a href='index.html' class='btn btn-primary'>Volver al inicio</a>"; // Enlace al index si faltan campos
     }
-}
-?>
+    ?>
