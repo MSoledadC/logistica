2fb6944f08dd7b048e0a235ab84c35e065160add
Soledad Cabanillas <mariasoledadcabanillas@hotmail.com>
2024-10-25 21:48:22 -0300

    se modifica nav se agrega boton, se modifican textos en nosotros, servicios, contacto, como funciona, testimonios, cotizacion se agrega input, modificando php

--- js/scrolllogo.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {
-    var header = document.querySelector(".site-navbar");
-    if (window.pageYOffset > 50) {  // Cambia 50 por la cantidad de scroll que prefieras
-        header.classList.add("scrolled");
-    } else {
-        header.classList.remove("scrolled");
-    }
-};
--- mail_cotizacion.php
+++ mail_cotizacion.php
@@ -1,6 +1,7 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST['fq_name'];
+    $empresa = $_POST['fq_empresa'];
     $email = $_POST['fq_email'];
     $telefono = $_POST['fq_phone'];
     $origen = $_POST['fq_origen'];
@@ -8,32 +9,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $detalle = $_POST['fq_detalle'];
 
     // Validar que todos los campos estén llenos
-    if (!empty($nombre) && !empty($email) && !empty($telefono) && !empty($origen) && !empty($destino) && !empty($detalle)) {
+    if (!empty($nombre) && !empty($empresa) && !empty($email) && !empty($telefono) && !empty($origen) && !empty($destino) && !empty($detalle)) {
         $to = "cotizaciones@logistica4d.com.ar";
         $subject = "Nueva solicitud de cotización";
-        
+
         // Crear el mensaje con todos los campos
         $message = "Nombre: $nombre\n";
+        $message .= "Empresa: $empresa\n"; // Incluir Empresa en el correo
         $message .= "Email: $email\n";
         $message .= "Teléfono: $telefono\n";
         $message .= "Origen del envío: $origen\n";
         $message .= "Destino del envío: $destino\n";
         $message .= "Detalles adicionales: $detalle\n";
-        
+
         // Configurar los encabezados
         $headers = "From: $email";
-        
-          // Enviar el correo
-          if (mail($to, $subject, $message, $headers)) {
+
+        // Enviar el correo
+
+        if (mail($to, $subject, $message, $headers)) {
             echo "Cotización enviada con éxito.<br>";
-            echo "<a href='index.html' class='btn btn-primary'>Volver al inicio</a>"; // Enlace al index
+            echo "<a href='index.html#cotizacion' class='btn btn-primary'>Volver al inicio</a>"; // Enlace al index
         } else {
             echo "Hubo un error al enviar la cotización.<br>";
-            echo "<a href='index.html' class='btn btn-primary'>Volver al inicio</a>"; // Enlace al index en caso de error
+            echo "<a href='index.html#cotizacion' class='btn btn-primary'>Volver al formulario</a>";
         }
-    } else {
-        echo "Por favor, complete todos los campos.<br>";
-        echo "<a href='index.html' class='btn btn-primary'>Volver al inicio</a>"; // Enlace al index si faltan campos
-    }
+        } else {
+            echo "Por favor, complete todos los campos.<br>";
+            echo "<a href='index.html#cotizacion' class='btn btn-primary'>Volver al formulario</a>"; // Enlace al index si faltan campos
+        }
 }
 ?>
